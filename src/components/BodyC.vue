f581e8dfaa2449e881ed9f9994b5637015cd57ad
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-11-28 18:05:24 +0100

    BodyC: Prova filtraggio select

--- src/components/BodyC.vue
+++ src/components/BodyC.vue
@@ -2,8 +2,8 @@
   <div id="BodyC" class="p-5">
     <div class="container d-flex align-items-center">
       <h3>Filtra:</h3>
-      <select class="form-select w-25 " aria-label="Default select example">
-        <option value="default">Tutti</option>
+      <select class="form-select w-25 " aria-label="Default select example" v-model="scelta" @click="test()">
+        <option value="">Tutti</option>
         <option v-for="(elem, index) in vettoreGeneri" :key="index">{{elem}}</option>     
       </select>
     </div>
@@ -26,7 +26,9 @@ export default {
   data() {
       return {
         arrayTest: [],
-        vettoreGeneri: []
+        vettoreGeneri: [],
+        vettoreFiltrato: [],
+        scelta: ""
       }
   },
   mounted()
@@ -34,21 +36,40 @@ export default {
     // console.clear();
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) =>
     {
-      this.arrayTest = response.data.response;
-      this.arrayTest[0].poster = "https://s7.orientaltrading.com/is/image/OrientalTrading/16_878d?$PDP_VIEWER_IMAGE$";
-      this.arrayTest[0].author = "Four Ducks Developers";
-      this.arrayTest[0].title = "What a DUCK?!";
-      this.arrayTest[0].year = "2022";
-
+    
       response.data.response.forEach(element => {
         if(!this.vettoreGeneri.includes(element.genre))
         {
           this.vettoreGeneri.push(element.genre)
         }
       });
+
+      this.arrayTest = response.data.response;
+      this.arrayTest[0].poster = "https://s7.orientaltrading.com/is/image/OrientalTrading/16_878d?$PDP_VIEWER_IMAGE$";
+      this.arrayTest[0].author = "Four Ducks Developers";
+      this.arrayTest[0].title = "What a DUCK?!";
+      this.arrayTest[0].year = "2022";
+    
     })
+  },
+  beforeUpdate()
+  {
+    if(this.scelta == '')
+      {
+        return this.arrayTest
+      }
+      else
+      {
+        this.vettoreFiltrato = 
+        this.arrayTest.filter((elem) =>
+        {
+          return elem.genre == this.scelta
+        });
+        return this.vettoreFiltrato
+      }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
